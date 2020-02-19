061c5f242ca7245935ef62c46c4aaf2bfc106f3e
JohnIrle <irlejohn@gmail.com>
2020-02-19 16:34:46 -0600

    Connect frontend and backend.

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
         </Switch>
       </div>
     </Router>
+
+
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
-const Home = () => {
-    return (
-        <h1>Hello from Home</h1>
-    )
+
+class Home extends React.Component {
+    state = { res: "home" }
+
+    componentDidMount() {
+        fetch('/rest')
+            .then((response) => {
+                return response.json();
+            })
+            .then((myJson) => {
+                this.setState({ res: myJson.message })
+            })
+    }
+
+    render() {
+        return (
+            <h1>Hello from {this.state.res}</h1>
+        )
+    }
 }
 
 export default Home;
