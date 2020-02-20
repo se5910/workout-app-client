b2518749461e48f6d5829edfb292b3b3166c9c09
JohnIrle <irlejohn@gmail.com>
2020-02-20 12:04:47 -0600

    add final new line setting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,21 @@
-import React from 'react';
-
+import React from "react";
 
 class Home extends React.Component {
-    state = { res: "home" }
+  state = { res: "home" };
 
-    componentDidMount() {
-        fetch('/rest')
-            .then((response) => {
-                return response.json();
-            })
-            .then((myJson) => {
-                this.setState({ res: myJson.message })
-            })
-    }
+  componentDidMount() {
+    fetch("/rest")
+      .then(response => {
+        return response.json();
+      })
+      .then(myJson => {
+        this.setState({ res: myJson.message });
+      });
+  }
 
-    render() {
-        return (
-            <h1>Hello from {this.state.res}</h1>
-        )
-    }
+  render() {
+    return <h1>Hello from {this.state.res}</h1>;
+  }
 }
 
 export default Home;
