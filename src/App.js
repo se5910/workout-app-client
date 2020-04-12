5fa185b35e3f71c599148a1119a0c8fb057831f8
JohnIrle <irlejohn@gmail.com>
2020-04-12 14:43:08 -0500

    Working conditional navbar.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Navbar from "./components/Nav";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
