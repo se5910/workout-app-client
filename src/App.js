8564fa0873941e81e353e301f46fb21d3c1bb050
JohnIrle <irlejohn@gmail.com>
2020-04-12 18:31:53 -0500

    Check token middleware

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./pages/Dashboard";
 import SignUp from "./pages/SignUp";
 import "./App.css";
 import { Provider } from 'react-redux';
+
 import store from "./store";
 import SecureRoute from './util/SecureRoute'
 import setJWTToken from './util/setJWTToken'
@@ -14,24 +15,28 @@ import jwt_decode from 'jwt-decode'
 import { SET_CURRENT_USER } from './actions/types';
 import { logout } from './actions/authActions'
 
-const jwtToken = localStorage.jwtToken
 
-if (jwtToken) {
-  setJWTToken(jwtToken)
-  const decoded_jwtToken = jwt_decode(jwtToken)
-  store.dispatch({
-    type: SET_CURRENT_USER,
-    payload: decoded_jwtToken
-  })
-
-  const currentTime = Date.now() / 1000
-  if (decoded_jwtToken.exp < currentTime) {
-    store.dispatch(logout())
-    window.location.href = "/"
-  }
-}
 
 function App() {
+
+  console.log("checking")
+  const jwtToken = localStorage.jwtToken
+
+  if (jwtToken) {
+    setJWTToken(jwtToken)
+    const decoded_jwtToken = jwt_decode(jwtToken)
+    store.dispatch({
+      type: SET_CURRENT_USER,
+      payload: decoded_jwtToken
+    })
+
+    const currentTime = Date.now() / 1000
+    if (decoded_jwtToken.exp < currentTime) {
+      store.dispatch(logout())
+      window.location.href = "/"
+    }
+  }
+
   return (
     <Provider store={store}>
       <Router>
