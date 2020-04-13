17b24445676f5467bbcea6a8525f1e4342d9b1fc
JohnIrle <irlejohn@gmail.com>
2020-04-12 19:29:31 -0500

    Fix middleware

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import jwtDecode from 'jwt-decode'
-import setJWTToken from "./util/setJWTToken"
+import { logout } from './actions/authActions'
 const initialState = {}
 const middleware = [thunk]
 
@@ -11,12 +11,18 @@ let store
 const ReactReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const checkTokenExpirationMiddleware = store => next => action => {
-  const token = localStorage.jwtToken
   console.log("from middleware")
-  if (jwtDecode(token).exp < Date.now() / 1000) {
+
+
+  const token = localStorage.jwtToken
+  if (token) {
+    if (jwtDecode(token).exp < Date.now() / 1000) {
+      next(action);
+
+      logout();
+      next(action);
+    }
     next(action);
-    localStorage.clear();
-    setJWTToken(false)
   }
   next(action);
 };
