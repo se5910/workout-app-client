a73ff883b9afceeca48ce073a1dfd47be38d1d47
JohnIrle <irlejohn@gmail.com>
2020-04-21 16:07:25 -0500

    Add first draft of hero image.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,67 @@
 import React from "react";
 import { connect } from 'react-redux'
+import { Paper } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container'
+
+import Shane from "../images/Shayne-workout-11.jpg";
+
+
+const useStyles = makeStyles(theme => ({
+  paper: {
+    textAlign: 'center',
+    height: "600px",
+    padding: 0,
+    background: `url(${Shane})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  h1: {
+    marginTop: '1em',
+    color: 'white',
+  },
+  image: {
+    width: "100%",
+  },
+  overlay: {
+    position: 'absolute',
+    zIndex: 0,
+
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: "1rem",
+    minWidth: '12rem',
+  },
+
+}))
+
 
 const Home = ({ auth, history }) => {
-
+  const classes = useStyles();
   if (auth.validToken) {
     history.push("/dashboard")
   }
 
-  return <h1>Hello, Please register or login</h1>;
+  return (
+    <Container className={classes.paper} maxWidth='lg'>
+      <Typography variant="h2" component="h2" className={classes.h1} responsive>
+        Hello Please register or login
+      </Typography>
+      <div className={classes.buttons}>
+        <Button variant="contained" color="secondary">Signup</Button>
+        <Button variant="contained" color="primary">Login</Button>
+      </div>
+    </Container>
+  )
 
 }
 
