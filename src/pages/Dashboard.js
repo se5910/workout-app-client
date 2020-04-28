34adc6028184320793d7f449bcc49abaf2f2daa2
JohnIrle <irlejohn@gmail.com>
2020-04-28 14:45:21 -0500

    Check for coach on page load for routing.

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,25 +3,28 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import CoachDashboard from "../components/Dashboards/CoachDashboard";
 import ClientDashboard from "../components/Dashboards/ClientDashboard";
+import { verifyCoach } from "../actions/authActions";
 
-const Dashboard = ({ auth: { user } }) => {
-    useEffect(() => {});
+const Dashboard = ({ auth: { user }, coach: { coach }, verifyCoach }) => {
+    useEffect(() => {
+        verifyCoach();
+    }, [verifyCoach]);
 
-    const { isCoach } = user;
     return (
-        <Fragment>
-            {isCoach ? <CoachDashboard /> : <ClientDashboard />}
-        </Fragment>
+        <Fragment>{coach ? <CoachDashboard /> : <ClientDashboard />}</Fragment>
     );
 };
 
 Dashboard.propTypes = {
     auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired,
+    coach: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
     auth: state.auth,
     profile: state.profile,
+    coach: state.coach,
 });
 
-export default connect(mapStateToProps)(Dashboard);
+export default connect(mapStateToProps, { verifyCoach })(Dashboard);
