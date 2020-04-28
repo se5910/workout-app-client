da548d0a82ef82362bcb81aa5f901e92d801e30f
JohnIrle <irlejohn@gmail.com>
2020-04-28 15:38:16 -0500

    Center spinner.

--- src/components/Layout/Spinner.js
+++ src/components/Layout/Spinner.js
@@ -1,18 +1,24 @@
 import React, { Fragment } from "react";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 const useStyles = makeStyles({
     spinner: {
-        margin: "auto",
+        margin: "0 auto",
+    },
+    center: {
+        display: "flex",
+        height: "25vw",
+        alignItems: "center",
     },
 });
 
 export const Spinner = () => {
     const classes = useStyles();
     return (
-        <Fragment>
+        <Container className={classes.center}>
             <CircularProgress className={classes.spinner} />
-        </Fragment>
+        </Container>
     );
 };
