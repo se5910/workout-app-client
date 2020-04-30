5a429fb207bb4da0614908269e9b666140ffc6d4
JohnIrle <irlejohn@gmail.com>
2020-04-30 15:50:12 -0500

    add getClient action.

--- src/actions/coachActions.js
+++ src/actions/coachActions.js
@@ -1,4 +1,4 @@
-import { GET_CLIENTS, GET_ERRORS } from "./types";
+import { GET_CLIENTS, GET_CLIENT, GET_ERRORS } from "./types";
 import axios from "axios";
 
 export const getClients = () => async (dispatch) => {
@@ -6,6 +6,23 @@ export const getClients = () => async (dispatch) => {
         const res = await axios.get("/api/coach/clients");
         dispatch({
             type: GET_CLIENTS,
+            payload: res.data,
+        });
+    } catch (err) {
+        if (err.response && err.response.data) {
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data,
+            });
+        }
+    }
+};
+
+export const getClient = (id) => async (dispatch) => {
+    try {
+        const res = await axios.get(`/api/client/${id}`);
+        dispatch({
+            type: GET_CLIENT,
             payload: res.data,
         });
     } catch (err) {
