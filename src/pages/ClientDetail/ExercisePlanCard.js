3450a2071279e4d0f2ce0906cc4c955ae00172cf
JohnIrle <irlejohn@gmail.com>
2020-04-30 16:02:10 -0500

    Initial Commit

---
+++ src/pages/ClientDetail/ExercisePlanCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const ExercisePlanCard = (props) => {
+    return <div></div>;
+};
+
+ExercisePlanCard.propTypes = {};
+
+export default ExercisePlanCard;
---
+++ src/pages/ClientDetail/MealPlanCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MealPlanCard = (props) => {
+    return <div></div>;
+};
+
+MealPlanCard.propTypes = {};
+
+export default MealPlanCard;
