9d85dbe74ebfb4a51588740a4d475beef5b82811
JohnIrle <irlejohn@gmail.com>
2020-05-01 23:52:34 -0500

    Map through plans and create a link for each plan.

--- src/pages/ClientDetail/ExercisePlanCard.js
+++ src/pages/ClientDetail/ExercisePlanCard.js
@@ -1,8 +1,45 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import { Link as RouterLink } from "react-router-dom";
 import PropTypes from "prop-types";
 
-const ExercisePlanCard = (props) => {
-    return <div></div>;
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275,
+        marginTop: "20px",
+    },
+    title: {
+        fontSize: 14,
+    },
+    item: {
+        marginBottom: 12,
+    },
+});
+
+const ExercisePlanCard = ({ plans, clientId }) => {
+    const classes = useStyles();
+
+    console.log(plans);
+
+    return (
+        <Card className={classes.root}>
+            <CardContent>
+                {plans &&
+                    plans.map((plan) => (
+                        <Typography
+                            variant="h5"
+                            component={RouterLink}
+                            to={`/client/${clientId}/exercise-plan/${plan.planId}`}
+                        >
+                            {plan.name}
+                        </Typography>
+                    ))}
+            </CardContent>
+        </Card>
+    );
 };
 
 ExercisePlanCard.propTypes = {};
