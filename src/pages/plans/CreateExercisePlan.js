8f5f1e5093fcf4f48598c3d04d1e0e21621d2a77
JohnIrle <irlejohn@gmail.com>
2020-05-04 16:09:22 -0500

    inital commit

---
+++ src/pages/plans/CreateExercisePlan.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CreateExercisePlan = (props) => {
+    return <div></div>;
+};
+
+CreateExercisePlan.propTypes = {};
+
+export default CreateExercisePlan;
---
+++ src/pages/plans/CreateMealPlan.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const CreateMealPlan = (props) => {
+    return <div></div>;
+};
+
+CreateMealPlan.propTypes = {};
+
+export default CreateMealPlan;
