e6990c33a13af7a71059ef2b7b5675ac5c8a5587
JohnIrle <irlejohn@gmail.com>
2020-05-05 00:23:29 -0500

    Map meal and exercise plans. '

--- src/components/Dashboards/ClientDashboard/ClientExercisePlans.js
+++ src/components/Dashboards/ClientDashboard/ClientExercisePlans.js
@@ -1,27 +1,57 @@
 import React, { useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import { getClientExercisePlans } from "../../../actions/planActions";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles({
+    content: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    margin: {
+        margin: ".5rem",
+        marginLeft: "1rem",
+    },
+});
 
 const ClientExercisePlans = ({
     profile: { profile },
+    plans: { exercisePlans },
     getClientExercisePlans,
 }) => {
+    const classes = useStyles();
     useEffect(() => {
         getClientExercisePlans(profile.id);
     }, [profile.id]);
     return (
-        <Paper>
-            <Typography variant="h5">Exercise Plans</Typography>
+        <Paper className={classes.content}>
+            <Typography variant="h5" className={classes.margin}>
+                Exercise Plans
+            </Typography>
             <hr />
+            {exercisePlans &&
+                exercisePlans.map((plan) => (
+                    <Typography
+                        variant="h5"
+                        className={classes.margin}
+                        key={plan.id}
+                        component={Link}
+                        to={`/client/${profile.id}/exercise-plan/${plan.planId}`}
+                    >
+                        {plan.name}
+                    </Typography>
+                ))}
         </Paper>
     );
 };
 
 const mapStateToProps = (state) => ({
     profile: state.profile,
+    plans: state.plans,
 });
 
 export default connect(mapStateToProps, { getClientExercisePlans })(
--- src/components/Dashboards/ClientDashboard/ClientMealPlans.js
+++ src/components/Dashboards/ClientDashboard/ClientMealPlans.js
@@ -1,24 +1,57 @@
 import React, { useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import { getClientMealPlans } from "../../../actions/planActions";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
-const ClientMealPlans = ({ profile: { profile }, getClientMealPlans }) => {
+const useStyles = makeStyles({
+    content: {
+        display: "flex",
+        flexDirection: "column",
+    },
+    margin: {
+        margin: ".5rem",
+        marginLeft: "1rem",
+    },
+});
+
+const ClientMealPlans = ({
+    profile: { profile },
+    plans: { mealPlans },
+    getClientMealPlans,
+}) => {
+    const classes = useStyles();
     useEffect(() => {
         getClientMealPlans(profile.id);
     }, [profile.id]);
     return (
-        <Paper>
-            <Typography variant="h5">Meal Plans</Typography>
+        <Paper className={classes.content}>
+            <Typography className={classes.margin} variant="h5">
+                Meal Plans
+            </Typography>
             <hr />
+            {mealPlans &&
+                mealPlans.map((plan) => (
+                    <Typography
+                        variant="h5"
+                        key={plan.id}
+                        className={classes.margin}
+                        component={Link}
+                        to={`/client/${profile.id}/exercise-plan/${plan.planId}`}
+                    >
+                        {plan.name}
+                    </Typography>
+                ))}
         </Paper>
     );
 };
 
 const mapStateToProps = (state) => ({
     profile: state.profile,
+    plans: state.plans,
 });
 
 export default connect(mapStateToProps, { getClientMealPlans })(
