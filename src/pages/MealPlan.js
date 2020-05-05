4456a1a0e86c8ebafc62e99d7334759af8e476b7
JohnIrle <irlejohn@gmail.com>
2020-05-04 19:00:18 -0500

    Initial Commit

---
+++ src/pages/MealPlan.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MealPlan = (props) => {
+    return <div></div>;
+};
+
+MealPlan.propTypes = {};
+
+export default MealPlan;
