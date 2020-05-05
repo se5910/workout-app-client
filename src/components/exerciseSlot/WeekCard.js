18982171429c4b3ed4a2e8442fe39069b5852612
JohnIrle <irlejohn@gmail.com>
2020-05-05 09:40:52 -0500

    Weekcard.

--- src/components/exerciseSlot/WeekCard.js
+++ src/components/exerciseSlot/WeekCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
@@ -7,6 +7,10 @@ import Typography from "@material-ui/core/Typography";
 import PropTypes from "prop-types";
 
 const WeekCard = ({ week }) => {
+    const [formData, setFormData] = useState({
+        setNumber: "",
+    });
+
     return (
         <Card>
             <Typography variant="p" style={{ textAlign: "center" }}>
@@ -14,10 +18,10 @@ const WeekCard = ({ week }) => {
             </Typography>
             <CardContent>
                 <form>
-                    <TextField size="small" />
-                    <TextField size="small" />
-                    <TextField size="small" />
-                    <TextField size="small" />
+                    <TextField size="small" label="Set Number" type="number" />
+                    <TextField size="small" label="Weight" type="number" />
+                    <TextField size="small" label="Reps" type="number" />
+                    <TextField size="small" label="RPE" type="number" />
                     <Button type="submit">save</Button>
                 </form>
             </CardContent>
