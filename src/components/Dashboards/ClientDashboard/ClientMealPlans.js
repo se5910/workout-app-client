d653cd3f4c8074d6278069d95cd53733163c3bff
JohnIrle <irlejohn@gmail.com>
2020-05-05 01:46:16 -0500

    Conditional rendering message for empty plans.

--- src/components/Dashboards/ClientDashboard/ClientMealPlans.js
+++ src/components/Dashboards/ClientDashboard/ClientMealPlans.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
 import { makeStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
 import { getClientMealPlans } from "../../../actions/planActions";
@@ -33,18 +34,23 @@ const ClientMealPlans = ({
                 Meal Plans
             </Typography>
             <hr />
-            {mealPlans &&
+            {mealPlans && mealPlans.length > 0 ? (
                 mealPlans.map((plan) => (
                     <Typography
                         variant="h5"
                         key={plan.id}
                         className={classes.margin}
                         component={Link}
-                        to={`/client/${profile.id}/exercise-plan/${plan.planId}`}
+                        to={`/client/${profile.id}/meal-plan/${plan.planId}`}
                     >
                         {plan.name}
                     </Typography>
-                ))}
+                ))
+            ) : (
+                <Container style={{ padding: ".5rem" }}>
+                    <Typography>No Meal plans have been created</Typography>
+                </Container>
+            )}
         </Paper>
     );
 };
